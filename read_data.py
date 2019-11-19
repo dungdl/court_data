52f3ee55b2daeaeb9c10935930c2c7081fbc5c63
dungdl28d11 <phongnt98@gmail.com>
2019-11-19 09:35:47 +0700

    convert data to .arff format

--- read_data.py
+++ read_data.py
@@ -20,6 +20,8 @@ decisions = df['decision']
 
 length = len(legal_relas) - 1
 
+distances = []
+
 for i in range(0, length):
     relation = legal_relas[i]
     decision = decisions[i]
@@ -30,13 +32,39 @@ for i in range(0, length):
     legal_relas[i] = corr_legal.group()
     decisions[i] = corr_decis.group()
 
+    distances.append(abs(plaintiff_ages[i] - defendant_ages[i])) 
+
+max_plaintiff = max(plaintiff_ages)
+max_defendant = max(defendant_ages)
+max_dist = max(distances)
+
+str_plaintiff = []
+str_defendant = []
+str_dist = []
+
+for i in range(18, max_plaintiff):
+    str_plaintiff.append(str(i))
+
+for j in range(18, max_defendant):
+    str_defendant.append(str(j))
+
+for k in range(1, max_dist):
+    str_dist.append(str(k))
+
+str_plaintiff = ','.join(str_plaintiff)
+str_defendant = ','.join(str_defendant)
+str_dist = ','.join(str_dist)
+
+print(str_plaintiff)
+print(str_defendant)
+print(str_dist)
 
 # MARK:- create list objects
 
 cases = []
 
 for i in range(0, length):
-    case = Case(ids[i], legal_relas[i], plaintiff_ages[i],defendant_ages[i], decisions[i])
+    case = Case(ids[i], legal_relas[i], plaintiff_ages[i], defendant_ages[i], decisions[i])
     cases.append(case)
 
 # MARK:- write to file
