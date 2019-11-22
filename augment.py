8f7d2d8a052e371aa488b275c33836770c70ff0a
dungdl28d11 <phongnt98@gmail.com>
2019-11-22 21:46:47 +0700

    data augmentation with countered data rules

--- augment.py
+++ augment.py
@@ -20,61 +20,71 @@ def new_case_one():
         output.append(str(self.decision))
     """
 
-    legal_rela = choice([4, 5, 7])
-    age_dist = 1
+    legal_rela = choice([4, 7, 8])
+    age_dist = choice([1, 2])
     decision = 2
     plaintiff_age = randint(23, 53)
     defendant_age = plaintiff_age + randint(1, 5)
 
     case = Case(1000, legal_rela, plaintiff_age, defendant_age, decision)
-    case.has_child = randint(0,1)
+
     return case
 
 
 def new_case_two():
-    legal_rela = choice([1, 8])
+    legal_rela = choice([1, 5])
     age_dist = 2
     decision = 2
-    plaintiff_age = randint(25, 53)
+    plaintiff_age = randint(18, 53)
     defendant_age = plaintiff_age + randint(6, 10)
 
     case = Case(1000, legal_rela, plaintiff_age, defendant_age, decision)
+    case.has_child = 0
     return case
 
+
 def new_case_three():
-    legal_rela = choice([2,3])
-    age_dist = 2
-    decision = 2
-    plaintiff_age = randint(25, 53)
+    legal_rela = choice([2, 3, 6])
+    age_dist = choice([1, 2])
+    decision = choice([1, 2])
+    plaintiff_age = randint(18, 53)
     defendant_age = plaintiff_age + randint(6, 10)
 
     case = Case(1000, legal_rela, plaintiff_age, defendant_age, decision)
+    case.has_child = choice([0,1])
     return case
 
 
 # MARK:- write to file
 file = open("cases.arff", "a")
-seed(1)
+seed(1, version=1)
 # generate some random numbers
 for _ in range(10):
     case = new_case_one()
     file.write(case.toString())
 
+seed(1, version=2)
 for _ in range(30):
     case = new_case_two()
     file.write(case.toString())
 
+seed(1, version=3)
+for _ in range(30):
+    case = new_case_three()
+    file.write(case.toString())
+seed(1, version=4)
 for _ in range(30):
     case = new_case_one()
     file.write(case.toString())
 
+seed(1, version=5)
 for _ in range(10):
     case = new_case_two()
     file.write(case.toString())
 
+seed(1, version=6)
 for _ in range(50):
     case = new_case_three()
     file.write(case.toString())
 
 file.close()
-
